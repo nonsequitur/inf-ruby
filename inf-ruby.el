0885009c114c8ddd8c83e5212f07a26e68928b05
Cornelius Mika <cornelius.mika@gmail.com>
2010-07-21 09:13:03 +0200

    Version 2.2

--- inf-ruby.el
+++ inf-ruby.el
@@ -6,7 +6,7 @@
 ;; URL: http://www.emacswiki.org/cgi-bin/wiki/RubyMode
 ;; Created: 8 April 1998
 ;; Keywords: languages ruby
-;; Version: 2.1
+;; Version: 2.2
 ;; Package-Requires: ((ruby-mode "1.1"))
 
 ;;; Commentary:
