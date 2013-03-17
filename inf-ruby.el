74e96360b1f3078d7d1f23431e06f5f71f912eb8
Justin Grimes <justinlgrimes@gmail.com>
2013-03-17 15:22:58 -0300

    Added macruby to inf-ruby-implementations
    Needs its own entry as macirb doesn't support the --inf-ruby-mode option

--- inf-ruby.el
+++ inf-ruby.el
@@ -54,7 +54,8 @@
   '(("ruby"     . "irb --inf-ruby-mode -r irb/completion")
     ("jruby"    . "jruby -S irb -r irb/completion")
     ("rubinius" . "rbx -r irb/completion")
-    ("yarv"     . "irb1.9 --inf-ruby-mode -r irb/completion"))
+    ("yarv"     . "irb1.9 --inf-ruby-mode -r irb/completion")
+    ("macruby"  . "macirb -r irb/completion"))
   "An alist of ruby implementations to irb executable names.")
 
 ;; TODO: do we need these two defvars?
