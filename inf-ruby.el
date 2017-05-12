9bd6d24aca9739264883fbd1d12e95493999f5fb
Dmitry Gutov <dgutov@yandex.ru>
2017-05-13 01:28:37 +0300

    inf-ruby-console-gem: Enable completion
    Closes #103.

--- inf-ruby.el
+++ inf-ruby.el
@@ -838,7 +838,9 @@ Gemfile, it should use the `gemspec' instruction."
                  (concat " -r " (file-name-sans-extension file)))
                files
                ""))))
-    (inf-ruby-console-run (concat base-command args) "gem")))
+    (inf-ruby-console-run (concat base-command args
+                                  " -r irb/completion")
+                          "gem")))
 
 (defun inf-ruby-console-racksh-p ()
   (and (file-exists-p "Gemfile.lock")
